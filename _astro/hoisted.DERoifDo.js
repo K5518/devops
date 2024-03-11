163b70a778ab471b265ca2144bbcf62f61bd604c
Kamran Ahmed <kamranahmed.se@gmail.com>
2024-03-11 13:00:41 +0000

    Updates

--- _astro/hoisted.DERoifDo.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.C0xP8dBu.js";import"./navigation.Bz95h8j7.js";import"./jwt.M546E6_Y.js";const t=document.getElementById("settings-menu"),e=document.getElementById("settings-menu-dropdown");t?.addEventListener("click",()=>{e?.classList.toggle("hidden")});document.addEventListener("click",n=>{t?.contains(n.target)||e?.classList.add("hidden")});
